28734a9e264a915c16e261b4ed935ed5b85767ae
cxw8942500 <2469199459@qq.com>
2020-11-26 13:44:28 +0800

    大事件首页完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -6,7 +6,28 @@ var baseURL = 'http://ajax.frontend.itheima.net'
 // var baseURL = 'http://ajax.frontend.itheima.net'
 
 //拦截所有的ajax请求，
-$.ajaxPrefilter(function(params) {
-    //拼接对应环境的服务器地址
+$.ajaxPrefilter(function (params) {
+    //1.拼接对应环境的服务器地址
     params.url = baseURL + params.url
+
+
+    //2.对需要权限的接口配置头信息
+    // 必须以/my/开头才行
+    if (params.url.indexOf('/my/') !== -1) {
+        params.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    //3.拦截所有的响应，判断身份认证信息
+    params.complete = function (res) {
+        console.log(res.responseJSON);
+        var obj = res.responseJSON
+        if (obj.status === 1 && obj.message === "身份认证失败！") {
+            //1.清空本地token
+            localStorage.removeItem('token')
+            //2.页面跳转
+            location.href = '/login.html'
+        }
+    }
 })
